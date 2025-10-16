b90ecdffe499a56666c5201a558d37c74e61bdd8
thaimaisju <yiem01@gmail.com>
2025-10-16 12:20:38 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    nodePolyfills({
+      // Whether to polyfill `node:` protocol imports
+      protocolImports: true,
+    })
+  ],
+  define: {
+    'global': 'globalThis',
+  },
+  resolve: {
+    alias: {
+      buffer: 'buffer',
+      process: 'process/browser',
+    }
+  },
   server: {
     port: 3000,
     proxy: {
