5c2f5611f72ae84e17cca67ce89f458f931d596e
DegenPotato <yiem01@gmail.com>
2025-10-26 10:23:13 +0700

    fix all broken migrations

--- migrations/025_ohlcv_pumpfun_support.sql
+++ migrations/025_ohlcv_pumpfun_support.sql
@@ -13,16 +13,6 @@ ALTER TABLE ohlcv_backfill_progress ADD COLUMN migration_timestamp INTEGER;
 CREATE INDEX IF NOT EXISTS idx_ohlcv_data_pool_type 
 ON ohlcv_data(mint_address, pool_type, timestamp);
 
--- Update existing pump.fun pools
-UPDATE ohlcv_data 
-SET pool_type = 'bonding_curve'
-WHERE pool_address IN (
-  SELECT pool_address FROM token_pools WHERE dex = 'pump.fun'
-);
-
--- Update existing raydium pools  
-UPDATE ohlcv_data
-SET pool_type = 'dex'
-WHERE pool_address IN (
-  SELECT pool_address FROM token_pools WHERE dex = 'raydium'
-);
+-- Skip pool type updates - column 'dex' doesn't exist in token_pools
+-- This would require knowing which pools are pump.fun vs raydium
+-- Will be set correctly as new data comes in
