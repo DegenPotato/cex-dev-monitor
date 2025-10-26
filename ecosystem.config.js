662dd6c92fad778d5ce2e5d8dfdaeac4418d5f5b
DegenPotato <yiem01@gmail.com>
2025-10-26 08:56:25 +0700

    fix PM2 .env loading

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,6 +6,7 @@ module.exports = {
     autorestart: true,
     watch: false,
     max_memory_restart: '1G',
+    env_file: '.env',
     env_production: {
       NODE_ENV: 'production',
       PORT: 3001
